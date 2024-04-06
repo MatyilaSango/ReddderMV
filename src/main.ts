6831ff41abdbc4f0aef1a9e15560c3b8f2fee0f1
Sango <60422984+MatyilaSango@users.noreply.github.com>
2024-04-06 20:21:29 +0200

    chore: adding ngrx devtools

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, isDevMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
@@ -8,6 +8,7 @@ import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
 import { provideState, provideStore } from '@ngrx/store';
 import { searchAccountReducer } from './app/states/search/search.reducer';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 if (environment.production) {
   enableProdMode();
@@ -19,6 +20,7 @@ bootstrapApplication(AppComponent, {
     provideIonicAngular(),
     provideRouter(routes),
     provideStore(),
-    provideState({name: 'searchAccount', reducer: searchAccountReducer})
-  ],
+    provideState({ name: 'searchAccount', reducer: searchAccountReducer }),
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })
+],
 });
