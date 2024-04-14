6ff36cb436b4d7e45e77ead3de5c8929d87ab674
Sango <60422984+MatyilaSango@users.noreply.github.com>
2024-04-14 17:22:24 +0200

    chore: update search page to load and handle searched accounts

--- src/app/search/search.page.ts
+++ src/app/search/search.page.ts
@@ -1,19 +1,49 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { CommonModule, AsyncPipe } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonList, IonItem, IonText } from '@ionic/angular/standalone';
 import { HeaderComponent } from "../header/header.component";
 import { SearchComponentComponent } from "../search-component/search-component.component";
+import { Store } from '@ngrx/store';
+import { AppState } from '../Store/App';
+import { Router } from '@angular/router';
+import { TOAST_MESSAGES, URL_PAGES } from '../enums/enums';
+import { SearchService } from './search.service';
+import { storeAccount } from '../Store/Actions/account.action';
+import { User } from '../types/types';
+import { selectSearchedAccounts } from '../Store/Selectors/search.selector';
 
 @Component({
     selector: 'app-search',
     templateUrl: './search.page.html',
     styleUrls: ['./search.page.scss'],
     standalone: true,
-    imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, HeaderComponent, SearchComponentComponent]
+    imports: [IonText, IonItem, IonList, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, HeaderComponent, SearchComponentComponent]
 })
 export class SearchPage{
+  searchService = inject(SearchService);
+  searchedAccounts: User[] = [];
+  router: Router;
+  isToastOpen: boolean;
+  toastMessage: string;
 
-  constructor() { }
+  constructor(private store: Store<AppState>) { 
+    this.router = new Router()
+    this.isToastOpen = false
+    this.toastMessage = ""
+    this.store.select(selectSearchedAccounts).forEach(a => {
+      this.searchedAccounts = a.accounts
+    })
+  }
 
+  async handleAccount(name: string, type: string){
+    const data = await this.searchService.getData(name, type)
+    this.store.dispatch(storeAccount(data))
+    if (data.isFound) {
+      this.router.navigate([URL_PAGES.Home])
+    } else {
+      this.toastMessage = TOAST_MESSAGES.searchAccountNotFound;
+      this.isToastOpen = true
+    }
+  }
 }
