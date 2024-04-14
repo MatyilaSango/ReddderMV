bbe33b9beb67605d7b0c81432dc995a9173dd9fe
Sango <60422984+MatyilaSango@users.noreply.github.com>
2024-04-14 17:24:01 +0200

    chore: added load more button to force load

--- src/app/posts-container/posts-container.component.ts
+++ src/app/posts-container/posts-container.component.ts
@@ -7,17 +7,17 @@ import { AppState } from '../Store/App';
 import { Store } from '@ngrx/store';
 import { Router } from '@angular/router';
 import { URL_PAGES } from '../enums/enums';
-import { selectAccount, selectType } from '../Store/Selectors/search.selector';
-import { SearchComponentService } from '../search-component/search-component.service';
-import { loadMoreSearchAccount } from '../Store/Actions/search.action';
-import { IonContent } from "@ionic/angular/standalone";
+import { selectAccount, selectType } from '../Store/Selectors/account.selector';
+import { IonContent, IonButton } from "@ionic/angular/standalone";
+import { SearchService } from '../search/search.service';
+import { loadMoreAccount } from '../Store/Actions/account.action';
 
 @Component({
     selector: 'app-posts-container',
     templateUrl: './posts-container.component.html',
     styleUrls: ['./posts-container.component.scss'],
     standalone: true,
-    imports: [IonContent, PostComponent, AsyncPipe, CommonModule]
+    imports: [IonButton, IonContent, PostComponent, AsyncPipe, CommonModule]
 })
 export class PostsContainerComponent {
   @ViewChild(IonContent) content!: IonContent
@@ -28,7 +28,7 @@ export class PostsContainerComponent {
   router: Router
   account: string = ""
   type: string = ""
-  searchService = inject(SearchComponentService);
+  searchService = inject(SearchService);
 
   constructor(private store: Store<AppState>) { 
     this.isLoadingMore = false
@@ -41,14 +41,14 @@ export class PostsContainerComponent {
     })
   }
 
-  async HandleLoadMorePosts() {
+  async HandleLoadMorePosts(forceloadMore?: boolean) {
     const contentScollElement = await this.content.getScrollElement()
     const scrollHeight = contentScollElement.scrollHeight
     const scrollTop = contentScollElement.scrollTop + contentScollElement.clientHeight
-    if(scrollHeight === scrollTop && (this.router.url === URL_PAGES.Home)){
+    if((scrollHeight === scrollTop || forceloadMore) && (this.router.url === URL_PAGES.Home)){
       this.isLoadingMore = true
       const data = await this.searchService.getData(this.account, this.type)
-      this.store.dispatch(loadMoreSearchAccount(data))
+      this.store.dispatch(loadMoreAccount(data))
       this.isLoadingMore = false
     }
   }
